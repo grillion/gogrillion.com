97f99573d2a5ea14da6763a91527e628bd34dcf4
Michael Grill <mgrill@ikanow.com>
2016-11-11 18:24:23 -0500

    Initial structure

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+<head>
+    <title>GoGrillion.com</title>
+    <link href="css/app.css" rel="stylesheet">
+    <script src="plugin/jquery/jquery.js"></script>
+    <script src="plugin/bootstrap/bootstrap.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row" id="logoRow">
+        <div class="col-xs-6 col-xs-offset-3">
+            <img src="images/gogrillion.svg">
+        </div>
+    </div>
+</div>
+</body>
+</html>
